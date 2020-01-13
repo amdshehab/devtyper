b32d866e69a26c2d25eafe43dc20f8a07d3fc904
Ahmed Shehab <shehab.ahmed.uk@gmail.com>
2020-01-13 21:50:58 +0000

    toggles character state based on user input

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.scss";
 import { useGenerateSnippet } from "./useGenerateSnippet";
 import { Character } from "./components/character";
@@ -14,11 +14,39 @@ function App() {
 
   const { currentSnippet, setSnippetIndex } = useGenerateSnippet(KeyboardKeys);
 
+  const [statefullSnippet, setStatefullSnippet] = useState(currentSnippet);
+
+  const [currentChar, setCurrentChar] = useState(0);
+
+  useEffect(() => {
+    const handleKeyPress = ({ key }) => {
+      console.log("currentChar", currentChar, key);
+
+      if (key === statefullSnippet[currentChar].character) {
+        statefullSnippet[currentChar].status = "CORRECT";
+        setStatefullSnippet([...statefullSnippet]);
+        setCurrentChar(char => char + 1);
+      } else {
+        statefullSnippet[currentChar].status = "INCORRECT";
+        setStatefullSnippet([...statefullSnippet]);
+      }
+    };
+
+    window.addEventListener("keydown", handleKeyPress);
+    return () => {
+      window.removeEventListener("keydown", handleKeyPress);
+    };
+  }, [statefullSnippet, currentChar]);
+
   return (
     <div className="App">
       <div className="Snippet">
-        {currentSnippet.map(({ character, status }) => (
-          <Character character={character} status={status} />
+        {statefullSnippet.map(({ character, status }, i) => (
+          <Character
+            character={character}
+            status={status}
+            key={character + i}
+          />
         ))}
       </div>
     </div>
--- src/useGenerateSnippet.js
+++ src/useGenerateSnippet.js
@@ -1,15 +1,22 @@
 import { useState } from "react";
 
 /**
- * @param {Array<String>} snippetList - user keyboard
+ * @param {String} character
+ */
+function Character(character) {
+  this.character = character;
+  this.status = "ACTIVE";
+}
+
+/**
+ * @param {Array<String>} snippetList
  */
 
 export const useGenerateSnippet = snippetList => {
   const [keyIndex, setSnippetIndex] = useState(0);
-  const currentSnippet = snippetList[keyIndex].split("").map(character => ({
-    character,
-    status: "ACTIVE"
-  }));
+  const currentSnippet = snippetList[keyIndex]
+    .split("")
+    .map(character => new Character(character));
 
   return {
     currentSnippet,
